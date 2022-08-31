08d95b8651f143e441838fe2ace4b0803edfcd10
victorvieiradev <contatovictorvieira@outlook.com>
2022-08-31 16:01:56 -0300

    método de exibir clientes na service finalizado

--- src/main/java/com/example/CURSO/service/UsuarioService.java
+++ src/main/java/com/example/CURSO/service/UsuarioService.java
@@ -1,4 +1,18 @@
 package com.example.CURSO.service;
 
+import com.example.CURSO.model.enumeric.UsuarioModel;
+import com.example.CURSO.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    public List<UsuarioModel> listarUsuariosCadastrados(){
+        return  usuarioRepository.findAll();
+    }
+
 }
