a4113ab20c848f38a6919bbd5a17b1cc23088827
victorvieiradev <contatovictorvieira@outlook.com>
2022-08-31 15:25:18 -0300

    estrutura inicial

---
+++ src/test/java/com/example/CURSO/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CURSO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
