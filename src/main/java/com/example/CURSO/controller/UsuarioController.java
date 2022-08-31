1ec15167c88c8dbbfa847a6df397da96ee5b5aa5
victorvieiradev <contatovictorvieira@outlook.com>
2022-08-31 18:20:07 -0300

    método de buscar por id finalizado no usuario controller

--- src/main/java/com/example/CURSO/controller/UsuarioController.java
+++ src/main/java/com/example/CURSO/controller/UsuarioController.java
@@ -1,4 +1,26 @@
 package com.example.CURSO.controller;
 
+import com.example.CURSO.model.enumeric.UsuarioModel;
+import com.example.CURSO.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+    @GetMapping(path = "/usuario")
+    public List<UsuarioModel> listarUsuariosCadastrados(){
+        return usuarioService.listarUsuariosCadastrados()
+    }
+    @GetMapping(path = "/usuario/{id}")
+    public Optional<UsuarioModel> buscarUsuarioPorId(@PathVariable Long id){
+        return usuarioService.buscarPorId(id);
+    }
+    
 }
