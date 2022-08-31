a3be69373f18b6f85725d8684d3105fb4920921d
victorvieiradev <contatovictorvieira@outlook.com>
2022-08-31 18:33:47 -0300

    adicionado forma de receber dados

--- src/main/java/com/example/CURSO/controller/UsuarioController.java
+++ src/main/java/com/example/CURSO/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.example.CURSO.controller;
 import com.example.CURSO.model.enumeric.UsuarioModel;
 import com.example.CURSO.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,10 @@ public class UsuarioController {
     @GetMapping(path = "/usuario/{id}")
     public Optional<UsuarioModel> buscarUsuarioPorId(@PathVariable Long id){
         return usuarioService.buscarPorId(id);
+    }
+    @DeleteMapping(path = "/usuario/{id}")
+    public void excluirUsuarioPorId(@PathVariable Long id){
+        usuarioService.excluirUsuarioPeloId(id);
     }
     
 }
---
+++ src/main/java/com/example/CURSO/model/enumeric/DadosUsuariosModel.java
@@ -0,0 +1,11 @@
+package com.example.CURSO.model.enumeric;
+
+import lombok.Data;
+
+@Data
+public class DadosUsuariosModel {
+    private Long id;
+    private String email;
+    private String senha;
+
+}
