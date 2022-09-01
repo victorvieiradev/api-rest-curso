eeb419e3438f700e138130cd372f0c8ca1a40047
victorvieiradev <contatovictorvieira@outlook.com>
2022-09-01 15:45:50 -0300

    adicionando o entity no ExibirUsuario.java

--- src/main/java/com/example/CURSO/model/enumeric/ExibirUsuario.java
+++ src/main/java/com/example/CURSO/model/enumeric/ExibirUsuario.java
@@ -1,8 +1,12 @@
 package com.example.CURSO.model.enumeric;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Data
+@Entity
 public class ExibirUsuario {
     private Long id;
     private String nome;
